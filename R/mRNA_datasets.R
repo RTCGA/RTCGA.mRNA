7b479acd70c5b5461a5c8775ac6e87b2513173c8
witia1990 <wc291520@students.mimuw.edu.pl>
2015-11-15 21:01:20 +0100

    Update of RTCGA.mrna package.

---
+++ R/mRNA_datasets.R
@@ -0,0 +1,95 @@
+#' 
+#' @title mRNA datasets from TCGA project
+#'
+#' @description
+#' Package provides mRNA datasets from The Cancer Genome Atlas Project for
+#' all cohorts types from \url{http://gdac.broadinstitute.org/}.
+#' Data were downloaded using \link{RTCGA-package} and contain snapshots for
+#' the date: \code{2015-08-21}. The process is described here:
+#' \url{http://mi2-warsaw.github.io/RTCGA.data/}.
+#' Use cases, examples and information about datasets in \pkg{RTCGA.data}
+#' family can be found here: \code{browseVignettes("RTCGA")}. mRNA data format
+#' is explained here \url{https://wiki.nci.nih.gov/display/TCGA/Clinical+Data+Overview}.
+#' 
+#' @details \code{browseVignettes("RTCGA")}
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' browseVignettes("RTCGA")
+#' }
+#'
+#' 
+#' @return Data frames with mRNA data.
+#' 
+#' @import RTCGA
+#' 
+#' @source \url{http://gdac.broadinstitute.org/}
+#' 
+#' @aliases BRCA.mRNA,COAD.mRNA,COADREAD.mRNA,GBMLGG.mRNA,KIPAN.mRNA,KIRC.mRNA,KIRP.mRNA,LGG.mRNA,LUAD.mRNA,LUSC.mRNA,OV.mRNA,READ.mRNA,UCEC.mRNA
+#' 
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#'
+"BRCA.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"COAD.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"COADREAD.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"GBMLGG.mRNA"  
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"KIPAN.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"KIRC.mRNA"    
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"KIRP.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"LGG.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"LUAD.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"LUSC.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"OV.mRNA"      
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"READ.mRNA"
+#' @name mRNA
+#' @rdname mRNA
+#' @format NULL
+#' 
+"UCEC.mRNA"
